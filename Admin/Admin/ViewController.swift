0793434572186a1a463fb78d398a31c1d3446fe7
imohamedmahgoub <imohamedmahgoub@gmail.com>
2024-09-02 00:18:22 +0300

    create xcode app

---
+++ Admin/Admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Admin
+//
+//  Created by Mohamed Mahgoub on 02/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
